b9f4d5dc51b0d1125310e010b990291c8d226ed9
kasparasdown <s235116@dtu.dk>
2024-09-10 11:27:01 +0200

    edited read_char and write_char to be functional

--- assignment_1/io.c
+++ assignment_1/io.c
@@ -6,8 +6,7 @@
 
 
 /* Reads next char from stdin. If no more characters, it returns EOF */
-int
-read_char() {
+int read_char() {
 
   char c;
   ssize_t readResult = read(STDIN_FILENO, &c, 1);
@@ -16,34 +15,38 @@ read_char() {
     return c;
   } else if(readResult == 0) {
     return EOF;
-  } else {
+  }
     return EOF;
   }
-}
 
 
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
-int
-write_char(char c) {
+int write_char(char c) {
   ssize_t readResult = write(STDOUT_FILENO, &c, 1);
   if (readResult ==1) {
     return 0;
   }
-  else {
-    return EOF;
-  }
+  return EOF;
 }
 
 /* Writes a null-terminated string to stdout.  If no errors occur, it returns 0, otherwise EOF */
-int
-write_string(char* s) {
+int write_string(char* s) {
+  ssize_t length = 0;
+
+  while (s[length] != '\0') {
+    length++;
+  }
+
+  ssize_t readResult = write(STDOUT_FILENO, s, length);
+  if(readResult == length) {
+    return 0;
+  }
   return EOF;
 }
 
 /* Writes n to stdout (without any formatting).   
  * If no errors occur, it returns 0, otherwise EOF
  */
-int
-write_int(int n) {
+int write_int(int n) {
   return EOF;
 }
