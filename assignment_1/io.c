5cc28768ab6be10be29b8f85a2bc892a29c50182
Setare Izadi <s232629@dtu.dk>
2024-09-10 11:17:53 +0200

    tilføjet task 1 write and read functions

--- assignment_1/io.c
+++ assignment_1/io.c
@@ -7,13 +7,30 @@
 /* Reads next char from stdin. If no more characters, it returns EOF */
 int
 read_char() {
-  return EOF;
+
+  char c;
+  ssize_t readResult = read(STDIN_FILENO, &c, 1);
+
+  if(readResult== 1) {
+    return c;
+  } else if(readResult == 0) {
+    return EOF;
+  } else {
+    return EOF;
+  }
 }
+
 
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_char(char c) {
-  return EOF;
+  ssize_t readResult = write(STDOUT_FILENO, &c, 1);
+  if (readResult ==1) {
+    return 0;
+  }
+  else {
+    return EOF;
+  }
 }
 
 /* Writes a null-terminated string to stdout.  If no errors occur, it returns 0, otherwise EOF */
