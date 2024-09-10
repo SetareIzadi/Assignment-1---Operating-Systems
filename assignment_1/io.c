bf163a916576f619858e206722597ec9c44e5747
kasparasdown <s235116@dtu.dk>
2024-09-10 11:12:37 +0200

    edited read_char and write_char to be functional

--- assignment_1/io.c
+++ assignment_1/io.c
@@ -1,19 +1,37 @@
 
 #include <errno.h>
 #include <unistd.h>
-
 #include "io.h"
+
+
 
 /* Reads next char from stdin. If no more characters, it returns EOF */
 int
 read_char() {
-  return EOF;
+
+  char c;
+  ssize_t readResult = read(STDIN_FILENO, &c, 1);
+
+  if(readResult== 1) {
+    return c;
+  } else if(readResult == 0) {
+    return EOF;
+  } else {
+    return EOF;
+  }
 }
+
 
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_char(char c) {
-  return EOF;
+  ssize_t readResult = write(STDOUT_FILENO, &c, 1);
+  if (readResult ==1) {
+    return 0;
+  }
+  else {
+    return EOF;
+  }
 }
 
 /* Writes a null-terminated string to stdout.  If no errors occur, it returns 0, otherwise EOF */
