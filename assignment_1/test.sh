0db7822081cca64123c76fbb0e23699972570fac
SarahDimi <144697325+SarahDimi@users.noreply.github.com>
2024-09-16 13:32:51 +0200

    Added 2 additional tests

--- assignment_1/test.sh
+++ assignment_1/test.sh
@@ -6,6 +6,18 @@ in="abbabaq"
 out="Count: 6
 Collection: 0,3,5"
 
-[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "Test Case 1: PASSED" || echo "Test Case 1: FAILED"
+
+# Test Case 2
+in="abccbaabcq"
+out="Count: 9
+Collection: 5"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "Test Case 2: PASSED" || echo "Test Case 2: FAILED"
+
+# Test Case 3
+in="aabcaacaq"
+out="Count: 8
+Collection: 0,4,7"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "Test Case 3: PASSED" || echo "Test Case 3: FAILED"
 
 
